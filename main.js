6d5c9eaaab4b9ca375798b744e1d8131e0ca65ce
Francisco <franciscoperezvivas96@gmail.com>
2025-10-15 12:06:33 -0600

    se agrego la operacion de multiplicacion

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ function resta(){
       let resultado = num1 - num2;
        return resultado;
 }// resta
+
+function producto(){
+      let resultado = num1 * num2;
+       return resultado;
+}// producto
+
