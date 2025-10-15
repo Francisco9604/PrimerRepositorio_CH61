4d409974e39b3f4e413e47043d4fd59355c3420e
Francisco <franciscoperezvivas96@gmail.com>
2025-10-15 11:39:39 -0600

    main.js

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function suma(){
       let resultado = num1 + num2;
        return resultado;
 }// suma
+
+
+function resta(){
+      let resultado = num1 - num2;
+       return resultado;
+}// resta
